7b479be367b495c75d228c8b63abcae0c8bef9d9
Ojkee <17oju17@gmail.com>
2025-11-15 16:11:56 +0100

    2025-11-15; Back; 1; CORS in FastAPI

--- backend/pkg/api.py
+++ backend/pkg/api.py
@@ -1,7 +1,6 @@
-import json
-from typing import NamedTuple
+from dataclasses import asdict, dataclass
+from typing import TypeAlias
 from backend.internal.lexing import Lexer
-from backend.internal.math_builtins.formula_entry import FormulaEntry
 from backend.internal.math_builtins.formula_handler import FORMULA_MAP
 from backend.internal.tokenstreams import TokenStream
 from backend.internal.parsing import Parser
@@ -29,31 +28,19 @@ def compile_math_input(input: str) -> list[str]:
     return [str(obj) for obj in result]
 
 
-def get_implemented_formulas_json() -> bytes:
-    Formula_tuple = NamedTuple(
-        "Formula_tuple",
-        [
-            ("display_name", str),
-            ("box_name", str),
-            ("latex_str", str),
-        ],
-    )
-    Category_tuple = NamedTuple(
-        "Category_tuple",
-        [
-            ("name", str),
-            ("formulas", list[Formula_tuple]),
-        ],
-    )
+FrontFormula: TypeAlias = dict[str, str]
+FrontFormulas: TypeAlias = dict[str, list[FrontFormula]]
 
-    def to_formula_tuple(fk: str, fv: FormulaEntry):
-        return Formula_tuple(fv.display_name, fk, fv.latex_str)
 
-    categories = []
-    for cat_name, forms in FORMULA_MAP.items():
-        form_tuples = [to_formula_tuple(*form) for form in forms.items()]
-        cat = Category_tuple(cat_name, form_tuples)
-        categories.append(cat)
+def get_implemented_formulas_json() -> FrontFormulas:
+    @dataclass(frozen=True)
+    class FormData:
+        pass
 
-    json_bytes = json.dumps(categories).encode("utf-8")
-    return json_bytes
+    def formula_dict() -> FrontFormula:
+        return asdict(FormData())
+
+    result: FrontFormulas = {}
+    for category_name, category in FORMULA_MAP.items():
+        pass
+    return result
--- backend/rest/router.py
+++ backend/rest/router.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from backend.rest.handlers import run
+
 
 def create_app() -> FastAPI:
     app = FastAPI(title="Interpreter Service")
 
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
     app.include_router(run.router)
+
     return app
+
